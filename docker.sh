802463313ed260c6bbf0f07f76fadd770f25ea35
Torrex Developer <torrex-project@ya.ru>
2021-02-13 19:27:03 +0000

    Add restart always.

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --net host -v "`pwd`:/code" -w /code --env-file .env -it publisher bash run.sh
+docker run -d --restart always --net host -v "`pwd`:/code" -w /code --env-file .env -it publisher bash run.sh
